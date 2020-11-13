465e956734ca7bd79dd923c4bdb2d2813cbab8a9
kevin janvier <kevin.janvier5@gmail.com>
2020-11-14 02:03:20 +0300

    initial push

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module birdeggsolution {
+
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+//    requires mysql.connector.java;
+
+
+    opens sample;
+}
